3646260f6b8160977711d7c3baffed048a9ca75b
Ivanildo Junior <ivanildoasj@gmail.com>
2020-05-01 14:56:26 +0000

    correcao erro

--- database/migrations/2020_04_06_114647_add_users_data.php
+++ database/migrations/2020_04_06_114647_add_users_data.php
@@ -17,7 +17,7 @@ class AddUsersData extends Migration
             $table->string('social_name');
             $table->char('cpf',11);
             $table->char('rg',12);
-            $table->date('date_emission');
+            $table->date('date_emission')->nullable($value = true);
             $table->char('org_emitter',45);
             $table->date('birth_day');
             $table->integer('role_id'); //Id Externo
@@ -25,7 +25,7 @@ class AddUsersData extends Migration
             $table->char('ctps_serie', 5);
             $table->char('pis', 11);
             $table->char('oab', 10);
-            $table->date('date_admission');
+            $table->date('date_admission')->nullable($value = true);
             $table->integer('address_id'); //Id Externo
             $table->integer('status_id'); //Id Externo
         });
@@ -42,7 +42,7 @@ class AddUsersData extends Migration
             $table->dropColumn('social_name')->default('');
             $table->dropColumn('cpf');
             $table->dropColumn('rg');
-            $table->dropColumn('date_emission')->nullable($value = true);
+            $table->dropColumn('date_emission');
             $table->dropColumn('org_emitter');
             $table->dropColumn('birth_day');
             $table->dropColumn('role_id'); //Id Externo
@@ -50,7 +50,7 @@ class AddUsersData extends Migration
             $table->dropColumn('ctps_serie');
             $table->dropColumn('pis');
             $table->dropColumn('oab');
-            $table->dropColumn('date_admission')->nullable($value = true);
+            $table->dropColumn('date_admission');
             $table->dropColumn('address_id'); //Id Externo
             $table->dropColumn('status_id'); //Id Externo
         });
